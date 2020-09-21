3e176c06ac7917bf1b93f4cc41fda382d8322c0b
dom <domepiro87@yahoo.it>
2020-09-21 16:18:27 +0200

    aggiunto_nuovo_js

---
+++ js/main2.js
@@ -0,0 +1,82 @@
+// FUNZIONI SCRIPT
+
+function renderCalendar(date) {
+    // copia dell'oggetto date per manipolarlo come voglio
+    var dateCalendar = moment(date);
+
+    // pulisco la lista
+    $("#days").html("");
+    // modifica intestazione mese
+    $("h1").text(dateCalendar.format("MMMM YYYY"));
+    // giorni del mese delle date passata come argomento della funzione
+    var dayInMonth = dateCalendar.daysInMonth();
+
+    // preparazione del template del <li> dei giorni
+    var source = $("#day-template").html();
+    var template = Handlebars.compile(source);
+
+    // creo ciclo per stampare i giorni del mese
+    for (var i = 1; i <= dayInMonth; i++) {
+
+        // creazione del content
+        var data = {
+            "day": i,
+            "month": dateCalendar.format("MMMM"),
+            "dateComplite": dateCalendar.format("YYYY-MM-DD")
+        };
+        //console.log(template(data));
+
+        // creazione del codice html del giorno
+        var html = template(data);
+        // inserimento del giorno nella pagina
+        $("#days").append(html);
+        // incrementiamo di 1 giorno la nostra data in date
+        dateCalendar.add(1, "days");
+    }
+}
+
+$(document).ready(function() {
+
+    var date = ("2018-01-01");
+    //var momentDate = moment("2018-01-01");
+    //console.log(momentDate.daysIsMonth());
+  
+    renderCalendar(date);
+
+    $.ajax(
+        {
+            "url": "https://flynn.boolean.careers/exercises/api/holidays",
+            "data": {
+                "year": 2018,
+                "month": date
+            },
+            "method": "GET",
+            "success": function(data) {
+                var response = data.response;
+                for (var i = 0; i < response.length; i++) {
+                    var dateHoliday = response[i].date;
+                    var nameHoliday = response[i].name;
+                    $(".day[data-giorno='"+ dateHoliday +"']").addClass("holiday");
+                    $(".day[data-giorno='"+ dateHoliday +"'] span").text(nameHoliday);
+                }
+
+            },
+            "error": function() {
+                alert("Errore!!");
+            }
+        }
+    );
+
+    $(".prev").click(function() {
+
+        if(date.format("M") == 1) {
+            alert("Non puoi andare indietro!!");
+        } else {
+            // spostare la data 1 mese indietro
+            date.subtract(1, 'months');
+            renderCalendar(date);
+        }
+
+    });
+
+});
